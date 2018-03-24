28326079fba5e6708fc63c0c36121af0a31b0689
mnap <learn@10g.pl>
2018-03-24 22:58:53 +0100

    Update scripts - extended version

--- js/script.js
+++ js/script.js
@@ -19,6 +19,13 @@ function searchCountries() {
 function showCountriesList(resp) {
     countriesList.empty();
     resp.forEach(function(item) {
-        $('<li>').text(item.name + ' | capital city: ' + item.capital).appendTo(countriesList).text();
+        var itemClass = item.alpha3Code;
+        $('<ul>').appendTo(countriesList).addClass(itemClass);
+        $('<li>').text(item.name).appendTo('.' + itemClass);
+        $('<li>').text('ISO codes: ' + itemClass + ', ' + item.alpha2Code)
+            .appendTo('.' + itemClass);
+        $('<li>').text('capital city: ' + item.capital)
+            .appendTo('.' + itemClass);
+        $('<img>').attr('src',item.flag).appendTo('.' + itemClass);
     });
 }
